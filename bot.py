14f9379694d4ae7afac4c84ba1c30c1d22f8b71f
sachin993986-maker <sachin993986@gmail.com>
2026-03-23 13:01:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
+import asyncio
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
-import os
-import asyncio
 
 TOKEN ="8718264717:AAHoPITsR4IfuqaU8VnHddAvb471nQqEmwg"
 
@@ -11,22 +11,23 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"Tumne likha: {update.message.text}")
 
-app = ApplicationBuilder().token(TOKEN).build()
+async def main():
+    app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT, reply))
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT, reply))
 
-print("Bot chal raha hai...")
+    print("Bot chal raha hai...")
 
-# 👇 IMPORTANT FIX
-async def run_bot():
     await app.initialize()
     await app.start()
     await app.updater.start_polling()
 
-    # keep running
+    # bot ko alive rakhne ke liye
     while True:
         await asyncio.sleep(10)
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(run_bot())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
