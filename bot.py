1c5094e1d2cbc82ba50a6d1226c03879020806a4
sachin993986-maker <sachin993986@gmail.com>
2026-03-23 12:49:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 import os
@@ -5,15 +6,19 @@ import os
 TOKEN ="8718264717:AAHoPITsR4IfuqaU8VnHddAvb471nQqEmwg"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello bhai! 🤖 Bot chal raha hai 🚀")
+    await update.message.reply_text("Hello bhai! 🤖 Bot live ho gaya 🚀")
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
-    await update.message.reply_text(f"Tumne likha: {text}")
+    await update.message.reply_text(f"Tumne likha: {update.message.text}")
 
-app = ApplicationBuilder().token(TOKEN).build()
+async def main():
+    app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT, reply))
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT, reply))
 
-app.run_polling()
+    print("Bot chal raha hai...")
+    await app.run_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
